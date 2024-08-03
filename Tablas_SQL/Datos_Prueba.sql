3cc68803d1b34211a82aca7b9208bef42865f11c
Joseph Suarez <161134965+Joseph-Suarez366@users.noreply.github.com>
2024-08-02 20:50:14 -0500

    Update Datos_Prueba.sql

--- Tablas_SQL/Datos_Prueba.sql
+++ Tablas_SQL/Datos_Prueba.sql
@@ -10,6 +10,14 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Volcando datos para la tabla credicel_ReportesmttoUPS.Clientes: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `Clientes` DISABLE KEYS */;
+INSERT INTO `Clientes` (`Id`, `Cliente`) VALUES
+	(1, 'Exito'),
+	(2, 'Falabella'),
+	(3, 'Homecenter');
+/*!40000 ALTER TABLE `Clientes` ENABLE KEYS */;
+
 -- Volcando datos para la tabla credicel_ReportesmttoUPS.Reporte: ~3 rows (aproximadamente)
 /*!40000 ALTER TABLE `Reporte` DISABLE KEYS */;
 INSERT INTO `Reporte` (`Id`, `Cliente`, `Oficina`, `Ubicacion`, `Fecha_Mtto`, `Equipo`, `Marca`, `Modelo`, `Potencia`, `Inventario`, `Serial`, `Temperatura`, `Alarmas`, `Fallas`, `Observaciones`) VALUES
@@ -17,6 +25,15 @@ INSERT INTO `Reporte` (`Id`, `Cliente`, `Oficina`, `Ubicacion`, `Fecha_Mtto`, `E
 	(4, 1, 'luis', 'luis', '2024-06-01', '', '', '', '', '11111', '222222', '', '', '', ''),
 	(5, 2, 'Cuarto electrico', '', '0000-00-00', '', '', '', '', '', '', '', '', '', '');
 /*!40000 ALTER TABLE `Reporte` ENABLE KEYS */;
+
+-- Volcando datos para la tabla credicel_ReportesmttoUPS.Tipo_Mtto: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `Tipo_Mtto` DISABLE KEYS */;
+INSERT INTO `Tipo_Mtto` (`Id`, `Mantenimiento`) VALUES
+	(1, 'Preventivo'),
+	(2, 'General'),
+	(3, 'Correctivo'),
+	(4, 'Instalacion');
+/*!40000 ALTER TABLE `Tipo_Mtto` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
